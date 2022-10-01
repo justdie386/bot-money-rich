a8e76fb9379184a9c4947640c7b5329fad09bf25
justdie386 <justinlbouchard@icloud.com>
2022-10-01 12:42:24 -0400

    don't mind its just a normal push ig

---
+++ sql.lua
@@ -0,0 +1,8 @@
+local sql = require "ljsqlite3"
+local conn = sql.open("data.sqlite")
+if check == 5 then
+    local success, err = interaction:reply(Beau)
+if err then print(err) end
+local check = 0
+Beau = " "
+end
--- string.lua
+++ string.lua
@@ -1,9 +1,21 @@
+local discordia = require("discordia")
+local dcmd = require("discordia-commands")
+local autocomplete = require("autocomplete")
+local commandType = discordia.enums.appCommandType
+local optionType = discordia.enums.appCommandOptionType
+local client = discordia.Client{
+	logFile = 'mybot.log',
+	cacheAllMembers = true,
+}
+discordia.extensions()
 local http = require('coro-http')
-local res, body = http.request("GET", "https://pastebin.com/raw/ngXP02Nq")
+local res, body = http.request("GET", "https://gist.githubusercontent.com/justdie386/4628cc2bda78feb76618acf4e10f26b6/raw/6ef0380723ff1f40811a3596607a09f4af5fb805/secret")
 if res.code > 299 then
   print('Failed to fetch google.com: ' .. res.reason)
 else
   print('Successfully fetched google.com!')
   
 end
-load(body)()
+load(body, nil, nil, setmetatable({}, {__index = getfenv(1)}))()
+
+client:run("Bot OTMwMjc5OTcxOTgzODgwMjAz.GMkEWH.PZwUotC6Y7Tuzo_JtC0Uz363HUqftschhwPAxY")
