733b2c12d984f38ae88ebe72032c60da3e166303
justdie386 <justinlbouchard@icloud.com>
2022-10-22 14:20:10 -0400

    nice

---
+++ simple.lua
@@ -0,0 +1,8 @@
+local dia = require("discordia")
+local dcmd = require("discordia-commands")
+local client = dia.Client
+
+client:on('messageCreate', function(msg) then
+end
+
+client:run("Bot NzQxMzI4ODk4NTU3NzM5MTUw.GkWl8o.qMrz14IEXXl-X9UA41Dt3SqruB043jy-m683o0")
